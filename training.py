bfca4092f0594b789545f15104b52d82beb4201f
Zayn Jarvis <zaynjarvis@outlook.com>
2017-11-12 12:39:16 +0800

    .

--- training.py
+++ training.py
@@ -1,17 +1,23 @@
 import tensorflow as tf
 import numpy as np
 import model as M
-from tensorflow.examples.tutorials.mnist import input_data
-import cv2 as cv
+from tensorflow.examples.tutorials.mnist import input_data 
+import cv2 as cv #                                         Hansel: "WHOEVER IS CODING, PLS USE TABS ONLY!!!!!!!!!!!"
 
 # create data reader #replace function read_data
 mnist = input_data.read_data_sets('MNIST_data',  one_hot = True)
 
 def build_model(input_data):
-	mod = M.Model(input_data, [None, 784])
-	mod.fcLayer(400, activation=M.PARAM_SIGMOID)
-	mod.fcLayer(400, activation=M.PARAM_SIGMOID)
-	mod.fcLayer(10) #no need activation;output layer always has no activation function
+	mod = M.Model(input_data, [None, 784]) #PEOPLE, -1 means None
+	mod.reshape([-1,28,28,1])#No need to input -1 # last param is channel
+	mod.convLayer(5, 6, activation=M.PARAM_RELU) # 5 is kernel size, 6 is channel
+	mod.maxpoolLayer(2)                   #samples a 2x2 area 
+	mod.convLayer(5, 16, activation=M.PARAM_RELU)
+	mod.maxpoolLayer(2)
+	mod.flatten()
+	# mod.fcLayer(50, activation=M.PARAM_RELU) 
+	mod.fcLayer(50, activation=M.PARAM_RELU) 
+	mod.fcLayer(10) #no need for activation;output layer always has no activation function
 	return mod.get_current_layer()
 
 def build_graph():
@@ -21,19 +27,23 @@ def build_graph():
 	loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=label_placeholder, logits=output))
 	accuracy = M.accuracy(output, tf.argmax(label_placeholder,1))
 	train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
-	return input_placeholder, label_placeholder, output, loss, accuracy, train_step
+	return input_placeholder, label_placeholder, output, loss, accuracy, train_step 
 
-MAX_ITER = 1000000
-BSIZE = 256
+MAX_ITER = 100000
 input_placeholder, label_placeholder, output, loss, accuracy, train_step = build_graph()
 
 with tf.Session() as sess:
+	saver = tf.train.Saver()
 	M.loadSess('./model/', sess, init = True) # ./model/ is directory of savings
-	for i in range(MAX_ITER):
-		x_train, y_train = mnist.train.next_batch(BSIZE)
+	for i in range(MAX_ITER):	
+		x_train, y_train = mnist.train.next_batch(128)
 		ls, acc, _ = sess.run([loss,accuracy,train_step], feed_dict={input_placeholder:x_train, label_placeholder:y_train})
 		if i % 100 == 0:
-			print('Iter : {}\t| acc : {:>.6f}\t| loss : {}'.format(i,acc,ls))                                            
-	if i % 1000 == 0:
-		acc = sess.run(accuracy,feed_dict={input_placeholder:mnist.test.images, label_placeholder:mnist.test.labels})
-		print('Test Accuracy : ', acc)
+			print('Iter : ',i,'\t| acc : ',acc,'\t| loss : ',ls)
+		if i % 1000 == 0:
+			acc = sess.run(accuracy,feed_dict={input_placeholder:mnist.test.images, label_placeholder:mnist.test.labels})
+			print('Test Accuracy : ', acc)
+
+	saver.save(sess, './model/digit.ckpt')
+
+# No bug version
